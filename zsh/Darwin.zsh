8abfc1b3a7e8c380a1ca590a7efe0a5514541251
tagawa <tagawa0525@gmail.com>
2011-08-05 20:29:10 +0900

    change from Mac Book Air

--- zsh/Darwin.zsh
+++ zsh/Darwin.zsh
@@ -1,7 +1,8 @@
 export LANG='ja_JP.UTF-8'
+export LC_ALL='ja_JP.UTF-8'
 export LSCOLORS='gxcxcxdxbxgegdabaeacad'
 #export PATH=/opt/local/bin:/opt/local/sbin:${PATH}
-export MANPATH=/opt/local/share/man:/opt/local/man:${MANPATH}
+#export MANPATH=/opt/local/share/man:/opt/local/man:${MANPATH}
 export CC='gcc'
 export CXX='g++'
 export LD_LIBRARY_PATH='/opt/local/lib'
